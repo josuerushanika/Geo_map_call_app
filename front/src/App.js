0e2168a18758dbd92d7b0d6c70fc3d79ded1bc31
josuerushanika <josuerushanika@gmail.com>
2023-06-07 14:46:12 +0200

    Fix some issues

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Routes,
-} from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import LoginPage from './LoginPage/LoginPage'
 import MapPage from './MapPage/MapPage'
 
