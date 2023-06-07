889700329756d113e0190f655adeed6f2794ff7e
josuerushanika <josuerushanika@gmail.com>
2023-06-07 17:24:18 +0200

    Upadte onclickhandle button

--- front/src/LoginPage/LoginPage.js
+++ front/src/LoginPage/LoginPage.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import './LoginPage.css'
+import { useNavigate } from 'react-router-dom'
 
 const Logo = () => {
       return <p className='logo'>
@@ -16,19 +17,25 @@ const LoginInput = (props) => {
      return <input className='l_page_input' value={username} onChange={handleValueChange}/>
 };
 
-const LoginButton = () => {
-    return <button className='l_page_login_button'>Login</button>
+const LoginButton = ({onClickHandler}) => {
+    return <button onClick={onClickHandler} className='l_page_login_button'>Login</button>
 }
 
 const LoginPage = () => {
     const [username, setUsername] = useState("");
+
+    const navigate = useNavigate();
+
+     const handleLogin = () => {
+        navigate('/map');
+     }
 
   return (
     <div className='l_page_main_container'>
          <div className='l_page_box'>
              <Logo/>
              <LoginInput username={username} setUsername={setUsername}/>
-             <LoginButton/>
+             <LoginButton onClickHandler={handleLogin}/>
          </div>
     </div>
   )
