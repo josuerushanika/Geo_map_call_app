b64f78e77b8dec3f9f4420aac6347ebd621a0ea4
josuerushanika <josuerushanika@gmail.com>
2023-06-07 15:43:03 +0200

    Update some change

--- front/src/LoginPage/LoginPage.js
+++ front/src/LoginPage/LoginPage.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './LoginPage.css'
 
+const Logo = () => {
+      return <p className='logo'>
+          GeoCall
+        </p>
+}
+
+const LoginInput = (props) => {
+    const { username, setUsername } = props;
+      const handleValueChange = (e) => {
+         setUsername(e.target.value);
+      }
+
+     return <input className='l_page_input' value={username} onChange={handleValueChange}/>
+};
+
+const LoginButton = () => {
+    return <button className='l_page_login_button'>Login</button>
+}
+
 const LoginPage = () => {
+    const [username, setUsername] = useState("");
+
   return (
     <div className='l_page_main_container'>
          <div className='l_page_box'>
-             Box
+             <Logo/>
+             <LoginInput username={username} setUsername={setUsername}/>
+             <LoginButton/>
          </div>
     </div>
   )
