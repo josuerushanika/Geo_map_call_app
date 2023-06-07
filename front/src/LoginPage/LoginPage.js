5ea6958acffa9edf001ce41a83877a8f431929a7
josuerushanika <josuerushanika@gmail.com>
2023-06-07 18:55:52 +0200

    Update location area

--- front/src/LoginPage/LoginPage.js
+++ front/src/LoginPage/LoginPage.js
@@ -1,6 +1,5 @@
-import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
-
+import React, {useEffect, useState} from 'react'
 import LoginButton from './LoginButton'
 import LoginInput from './LoginInput'
 import Logo from './Logo'
@@ -12,9 +11,27 @@ const isUsernameValid = (username) => {
     return username.length > 0 && username.length < 10 && !username.includes(' ');
 }
 
+const locationOptions = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+const onSucess = (position) => {
+  console.log(position);
+}
+
+const onError = (error) => {
+    console.log("Error occurred when trying to get location");
+    console.log(error);
+  }
 
 
 const LoginPage = () => {
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(onSucess, onError, locationOptions);
+     }, [])
+     
     const [username, setUsername] = useState("");
 
     const navigate = useNavigate();
