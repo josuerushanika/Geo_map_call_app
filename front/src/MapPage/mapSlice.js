d44d34004d738945873e90adabab1ad923353c04
josuerushanika <josuerushanika@gmail.com>
2023-06-07 14:08:35 +0200

    Update some map slice

---
+++ front/src/MapPage/mapSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    myLocation: null,
+    onlineUsers: [],
+    cardChosenOption: null
+};
+
+export const mapSlice = createSlice({
+    name: 'map',
+    initialState,
+    reducers: {
+        setMyLocation: (state, action) => {
+            state.myLocation = action.payload;
+        }
+    }
+});
+
+export const { setMyLocation } = mapSlice.actions;
