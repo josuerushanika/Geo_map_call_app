e9b70fb54f306e00894d1e9a1c2a1ad19535777c
josuerushanika <josuerushanika@gmail.com>
2023-06-07 12:31:18 +0200

    Add some testing connectivity

--- server/index.js
+++ server/index.js
@@ -2,14 +2,27 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+const { Server } = require('socket.io');
 
 const server = http.createServer(app);
 
 app.use(cors());
 
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+
 app.get('/', (req, res) => {
     res.send('Hello server is started');
 });
+
+io.on("connection", (socket) => {
+  console.log(`User Connected of the id: ${socket.id}`);
+});
+
 
 const PORT = process.env.PORT || 3003;
 
