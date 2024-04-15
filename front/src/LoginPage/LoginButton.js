e4332f214121ae613ac6c064e2c05d5e098781b3
josue rushanika <100031798+josuerushanika@users.noreply.github.com>
2024-04-15 08:51:48 +0200

    Update LoginButton.js

--- front/src/LoginPage/LoginButton.js
+++ front/src/LoginPage/LoginButton.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+const LoginButton = ({ onClickHandler, disabled }) => {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClickHandler}
+      className="l_page_login_button"
+    >
+      Login
+    </button>
+  );
+};
+
 
 
 export default LoginButton;
